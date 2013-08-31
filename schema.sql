18040dce106c9c228869a07d29a19a85fcfc2a1d
ravenjohn <rjlagrimas08@gmail.com>
2013-08-31 14:23:33 +0800

    Added schema

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+	id varchar(32) primary key,
+	access_token varchar(40) NOT NULL,
+	name varchar(32) not null,
+	password varchar(32),
+	type enum('visitor','admin','superadmin') default 'visitor' not null,
+	affiliation varchar(128),
+	country varchar(64),
+	category varchar(32),
+	contact varchar(128),
+	visit_count int(11) default 0 not null,
+	date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
+	date_updated TIMESTAMP null
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE logs(
+	id varchar(32) NOT NULL,
+	uri varchar(255) NOT NULL,
+	method varchar(6) NOT NULL,
+	params text,
+	access_token varchar(40) NOT NULL,
+	user_id varchar(40) NOT NULL,
+	ip_address varchar(45) NOT NULL,
+	authorized tinyint(1) NOT NULL,
+	date_created int(11) NOT NULL,
+	date_updated int(11) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
